6c925e415642d99eab884386e9c9c462454d0bca
One Mark <lakshsolanki848@gmail.com>
2025-08-02 15:30:16 +0530

    accurx

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-      <router-link class="navbar-brand" to="/">Accurs</router-link>
+      <router-link class="navbar-brand" to="/">Accurx</router-link>
       <button
         class="navbar-toggler"
         type="button"
